2c2620a12c4ebe11032e9b8810681e7118d5cf67
Aklil Zenebe Kiflay <56843738+azkiflay@users.noreply.github.com>
2025-07-30 13:41:31 +0100

    README

--- main.tf
+++ main.tf
@@ -1,14 +1,29 @@
 provider "aws" {
         region = "us-east-1"
     }
-    resource "aws_instance" "moodle_2" {
+    resource "aws_instance" "moodle" {
         ami = "ami-000d841032e72b43c" # --> ubuntu-2004-vm-circleci-classic-1727797387 # UEFI boot required --> "ami-00015f5b5bf56e076" # More ami's https://aws.amazon.com/marketplace
         instance_type = "t2.micro" # More at https://aws.amazon.com/ec2/instance-types/
-
+        vpc_security_group_ids = [aws_security_group.moodle_sg.id]
         tags = {
-            Name = "moodle_2"
+            Name = "moodle"
         }
+
+        user_data = <<-EOF
+            #!/bin/bash
+            echo "Hello, World" > index.html 
+            nohup busybox httpd -f -p 8080 & 
+            EOF
+
+        user_data_replace_on_change = true # --> forcefully terminate the original instance and create a new one to ensure user data script is executed.
     }
-
-
-
+    resource "aws_security_group" "moodle_sg" { 
+        name = "moodle_sg_allow_tcp_8080"
+        description = "Allow TCP traffic on port 8080"
+        ingress { 
+            from_port = 8080
+            protocol = "tcp"
+            to_port = 8080
+            cidr_blocks = ["0.0.0.0/0"] # Allow traffic from all possible IP addresses
+            }
+    }

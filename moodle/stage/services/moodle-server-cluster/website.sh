8be7879ab49ab85343233c8cd4cb778a34823aeb
Aklil Zenebe Kiflay <56843738+azkiflay@users.noreply.github.com>
2025-08-26 11:57:50 +0100

    .gitignore

--- moodle/stage/services/moodle-server-cluster/website.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -x
-set -e
-# Force non-interactive frontend for all apt commands
-echo 'export DEBIAN_FRONTEND=noninteractive' | sudo tee -a /etc/environment
-
-# Install nginx
-sudo apt-get -y install nginx
-sudo mkdir -p /var/www/website
-sudo sh -c "echo '<html><h1>This is a website</h1></html>' > /var/www/website/index.html"
